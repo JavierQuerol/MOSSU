2886d9e1ce0975e82530b0d3c5717143f7af1772
Javier Querol <querol.javi@gmail.com>
2025-07-16 15:55:31 +0200

    fixing deploy.sh and autoincremental version

--- deploy.sh
+++ deploy.sh
@@ -9,9 +9,27 @@ ARCHIVE_PATH="build/${APP_NAME}.xcarchive"
 EXPORT_PATH="build/export"
 APP_PATH="${EXPORT_PATH}/${APP_NAME}.app"
 PUBLIC_PATH="public"
+PLIST_PATH="MOSSU macos/Info.plist"
 ZIP_PATH="${PUBLIC_PATH}/${APP_NAME}.zip"
 SIGN_IDENTITY="Apple Development: Javier Querol Morata (VWXPT76R65)"
 DEPLOYMENT_SERVER="Vercel"
+
+# 1. Incrementar minor version
+CURRENT_VERSION=$(/usr/libexec/PlistBuddy -c "Print CFBundleShortVersionString" "$PLIST_PATH")
+
+IFS='.' read -r MAJOR MINOR <<< "$CURRENT_VERSION"
+NEW_MINOR=$((MINOR + 1))
+NEW_VERSION="$MAJOR.$NEW_MINOR"
+
+echo "Bumping version from $CURRENT_VERSION to $NEW_VERSION"
+/usr/libexec/PlistBuddy -c "Set :CFBundleShortVersionString $NEW_VERSION" "$PLIST_PATH" #> /dev/null
+
+# 2. Incrementar build number con agvtool
+xcrun agvtool bump -all > /dev/null
+
+# 3. (Opcional) Confirmar los cambios
+NEW_BUILD=$(/usr/libexec/PlistBuddy -c "Print CFBundleVersion" "$PLIST_PATH")
+echo "BUILD: $NEW_BUILD"
 
 echo "🛠 Archivando la app..."
 xcodebuild -quiet -scheme "$SCHEME" \
@@ -45,3 +63,7 @@ git commit -m "new version of $APP_NAME"
 git push
 
 echo "Servidor $DEPLOYMENT_SERVER actualizado "
+
+
+
+
